6ae9b1c12561128999f0854af0a92b924235be6d
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-09-09 22:15:10 +0200

    header footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ToastContainer position="bottom-center" limit={1} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
